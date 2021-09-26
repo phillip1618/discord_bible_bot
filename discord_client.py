aa27735d6697642ef6923d7f0249398e55a0d490
Phillip Kim <phillipkakg@gmail.com>
2021-09-25 20:50:06 -0700

    Placed retrieval of Verse of the Day in a separate DiscordClient class method. Also added votd attribute to that class

--- discord_client.py
+++ discord_client.py
@@ -6,6 +6,8 @@ class DiscordClient(discord.Client):
     def __init__(self):
         super().__init__()
 
+        self.votd = self.get_votd()
+
     async def on_ready(self):
         print('We have logged in as {0.user}'.format(self))
 
@@ -14,14 +16,16 @@ class DiscordClient(discord.Client):
             return
 
         if message.content.startswith('$votd'):
-            votd_response = requests.get('https://www.biblegateway.com/votd/get/?format=json&version=ESV&callback=BG.votdWriteCallback')
-            votd_text = votd_response.text
-            votd_string_dict = votd_text[21:-2]
-            votd_dict = ast.literal_eval(votd_string_dict)
 
-            votd_verse = votd_dict['votd']['text'][7:-7]
-            votd_ref = votd_dict['votd']['display_ref']
-            
-            votd_final = '"' + votd_verse + '" -' + votd_ref
+            await message.channel.send(self.votd)
 
-            await message.channel.send(votd_final)
+    def get_votd(self):
+        votd_response = requests.get('https://www.biblegateway.com/votd/get/?format=json&version=ESV&callback=BG.votdWriteCallback')
+        votd_text = votd_response.text
+        votd_string_dict = votd_text[21:-2]
+        votd_dict = ast.literal_eval(votd_string_dict)
+        votd_verse = votd_dict['votd']['text'][7:-7]
+        votd_ref = votd_dict['votd']['display_ref']  
+        votd_final = '"' + votd_verse + '" -' + votd_ref
+
+        return votd_final
