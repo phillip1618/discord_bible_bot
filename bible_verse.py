5226202fba46b0bc7e2d3ab8935a2c6f7b5e3162
Phillip Kim <phillipkakg@gmail.com>
2021-10-06 11:04:20 -0700

    Added separate_query method and replaced format_query method with get_url in BibleVerse

--- bible_verse.py
+++ bible_verse.py
@@ -1,13 +1,28 @@
+from urllib.request import urlopen
+
 class BibleVerse:
     def __init__(self):
         self.verse = self.get_verse()
 
-    def format_query(self, query):
-        query = query.replace(" ", "")
-        query = query.replace(":", "%3A")
-        query = query.replace(",", "%2C")
+    def separate_query(self, query):
+        search = query[7:]
+        search = search.replace(" ", "")
+        search_components = search.split("!")
 
-        return query
+        return search_components
+
+    
+    def get_url(self, search_components):
+
+        verses = search_components[0]
+        version = search_components[1]
+
+        verses = verses.replace(":", "%3A")
+        verses = verses.replace(",", "%2C")
+
+        url = "https://www.biblegateway.com/passage/?search=" + verses + "&version=" + version
+
+        return url
 
     def get_html(self, url):
         return
@@ -17,4 +32,7 @@ class BibleVerse:
 
 if __name__ == '__main__':
     x = BibleVerse()
-    print(x.format_query("Genesis 1:1-9, John 1:1-9"))
+    y = x.separate_query('#search Genesis 1:1-9, John 1:1-9!ESV')
+    print(y)
+    z = x.get_url(y)
+    print(z)
