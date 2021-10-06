d9c30c508c7ca2b7f773c15e9686522aa1d5172a
Phillip Kim <phillipkakg@gmail.com>
2021-10-06 10:41:19 -0700

    Added new BibleVerse class for retrieving Bible verses

---
+++ bible_verse.py
@@ -0,0 +1,12 @@
+class BibleVerse:
+    def __init__(self):
+        self.verse = self.get_verse()
+
+    def format_query(self, query):
+        return
+
+    def get_html(self, url):
+        return
+
+    def get_verse(self, html):
+        return
