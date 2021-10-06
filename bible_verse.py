dc380cf8759190d81d75cebc11bcbca72a274531
Phillip Kim <phillipkakg@gmail.com>
2021-10-06 10:48:20 -0700

    Added a format_query method to the BibleVerse class

--- bible_verse.py
+++ bible_verse.py
@@ -3,10 +3,18 @@ class BibleVerse:
         self.verse = self.get_verse()
 
     def format_query(self, query):
-        return
+        query = query.replace(" ", "")
+        query = query.replace(":", "%3A")
+        query = query.replace(",", "%2C")
+
+        return query
 
     def get_html(self, url):
         return
 
-    def get_verse(self, html):
+    def get_verse(self):
         return
+
+if __name__ == '__main__':
+    x = BibleVerse()
+    print(x.format_query("Genesis 1:1-9, John 1:1-9"))
