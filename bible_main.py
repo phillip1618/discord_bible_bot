4a3854eeeab0f9d7ea12353a87eaa6ce7a780eea
Phillip Kim <phillipkakg@gmail.com>
2022-03-21 22:53:02 -0700

    Added some different options for tokens in main script

--- bible_main.py
+++ bible_main.py
@@ -10,4 +10,9 @@ if (os.getenv('SENTRY_PROD') == 'PROD'):
     sentry_sdk.init(os.getenv('SENTRY_SDK_INIT'), traces_sample_rate=1.0)
 
 client = DiscordClient()
-client.run(os.getenv('TOKEN'))
+
+prod_bool = os.getenv('PROD')
+if prod_bool == 'True':
+    client.run(os.getenv('TOKEN_PROD'))
+else:
+    client.run(os.getenv('TOKEN_DEV'))
