d33f2812feffe54fd8b1422fc7e4c9efbd2b249d
Phillip Kim <phillipkakg@gmail.com>
2022-03-26 15:09:27 -0700

    Added a replace_unicode_decimals_codes function to VOTD class

--- votd.py
+++ votd.py
@@ -3,6 +3,7 @@ import discord
 import requests
 import ast
 import os
+import re
 
 import datetime
 import pytz
@@ -14,16 +15,27 @@ class VOTD:
     def __init__(self):
         self.votd = self.get_votd()
 
+    def replace_unicode_decimals_codes(self, votd_verse):
+        replace_bool = True
+
+        while replace_bool:
+            if '&#' in votd_verse:
+                index = votd_verse.find('&#')
+                decimal_code = int(votd_verse[index+2:index+6])
+                character = chr(decimal_code)
+                votd_verse = votd_verse.replace(votd_verse[index:index+6], character)
+            else:
+                replace_bool = False
+
+        return votd_verse
+    
     def get_votd(self):
         votd_response = requests.get('https://www.biblegateway.com/votd/get/?format=json&version=ESV&callback=BG.votdWriteCallback')
         votd_response_text = votd_response.text
         votd_json_str = votd_response_text[21:-2]
         votd_json = ast.literal_eval(votd_json_str)
         votd_verse = votd_json['votd']['text'][7:-7]
-
-        if "&#8220" or "&#8221" in votd_verse:
-            votd_verse = votd_verse.replace("&#8220;", "'")
-            votd_verse = votd_verse.replace("&#8221;", "'")
+        votd_verse = self.replace_unicode_decimals_codes(votd_verse)
 
         votd_reference = votd_json['votd']['display_ref']
         votd_final = '"' + votd_verse + '" -' + votd_reference
@@ -38,3 +50,8 @@ class VOTD:
                 self.votd = self.get_votd()
                 webhook.send(self.votd)
                 sleep(61)
+
+
+if __name__ == '__main__':
+    votd = VOTD()
+    print(votd.replace_unicode_decimals_codes('I am a jaguar&#8220 and I like pie &#8224!'))
